a2238c51a344db04c63a3c0e60c7347aa02a6e02
roblot <f.roblot.coulanges@gmail.com>
2025-02-25 15:48:30 +0100

    modif section portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -89,18 +89,19 @@ function App() {
       <section id="portfolio" className="py-20">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-16">Mes Projets</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+            
+            <ProjectCard 
+              title="Personal Training"
+              image="/images/fredlecoach.png"
+              tech="JS | React.js | Node.js | Express | JQuery "
+              link="https://fredlecoach.netlify.app/"
+            />
             <ProjectCard 
               title="Tempo : The To Do List"
               image="/images/logoTempo.png"
               tech="JS | Vue.js | API Rest | Bootstrap"
               link="https://tempo-the-todolist.netlify.app/"
-            />
-            <ProjectCard 
-              title="Personal Training"
-              image="/images/fredlecoach.png"
-              tech="JS | React.js | Node.js | Express | JQuery | Bootstrap"
-              link="https://fredlecoach.netlify.app/"
             />
             <ProjectCard 
               title="Les jolies sorcières"
@@ -114,7 +115,7 @@ function App() {
               tech="JS | React.js | HTML | CSS | Bootstrap"
               link="https://fred-webdev-smart-travel.netlify.app/"
             />
-            <ProjectCard 
+            {/* <ProjectCard 
               title="Chambres d'hôtes"
               image="/images/logo-2.png"
               tech="PHP | Symfony | JS | Bootstrap"
@@ -125,7 +126,7 @@ function App() {
               image="/images/logo_credit-finance.png"
               tech="PHP | Symfony | JS | HTML | CSS"
               link="https://fred-webdev.fr/"
-            />
+            /> */}
           </div>
         </div>
       </section>
